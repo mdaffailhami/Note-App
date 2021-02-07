28af3e132c86d8376416d472e05fbfd1d77b76ce
Daffa Ilhami <74972129+mdaffailhami@users.noreply.github.com>
2021-02-07 09:54:50 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * @author [Muhammad Daffa Ilhami]
+ * @email [mdaffailhami@gmail.com]
+ * @create date 2021-02-04 | 19:53:22
+ * @modify date 2021-02-06 | 21:21:16
+ * @desc [description]
+ */
+
+const mod = {
+  express: require("express"),
+  session: require("express-session"),
+};
+
+const routers = {
+  index: require("./pages/index/router.js"),
+  login: require("./pages/login/router"),
+  register: require("./pages/register/router.js"),
+  create: require("./pages/create/router.js"),
+  update: require("./pages/update/router.js"),
+};
+
+const database = require("./database/note-app.js");
+const app = mod.express();
+
+// Session
+app.use(
+  mod.session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true },
+  })
+);
+
+// Routes
+app.use(routers.index);
+app.use(routers.login);
+app.use(routers.register);
+app.use(routers.create);
+app.use(routers.update);
+
+app.listen(1234, console.log("Server is running on http://localhost:1234"));
---
+++ database/note-app.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  `mongodb+srv://daffa-ilhami:7jfegpsRVmwtosk1@cluster0.wuxkd.mongodb.net/note-app?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) throw err;
+    console.log("Database terkoneksi!!!");
+  }
+);
+
+// Collections
+
+const Account = mongoose.model(
+  "accounts",
+  new mongoose.Schema({
+    username: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  })
+);
+
+const Note = mongoose.model(
+  "notes",
+  new mongoose.Schema({
+    user__id: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+    content: {
+      type: String,
+      require: true,
+    },
+  })
+);
+
+module.exports = {
+  collections: {
+    Account,
+    Note,
+  },
+};
+
+// const mongoose = require("mongoose");
+
+// const connection = () => {
+//   return mongoose.connect(
+//     `mongodb+srv://daffa-ilhami:7jfegpsRVmwtosk1@cluster0.wuxkd.mongodb.net/note-app?retryWrites=true&w=majority`,
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     (err) => {
+//       if (err) throw err;
+//       console.log("Database terkoneksi!!!");
+//     }
+//   );
+// };
+
+// const collection = () => {
+//   return mongoose.model(
+//     "users",
+//     new mongoose.Schema({
+//       note: {
+//         type: String,
+//         required: true,
+//       },
+//     })
+//   );
+// };
+
+// module.exports = {
+//   connection,
+//   collection,
+// };
